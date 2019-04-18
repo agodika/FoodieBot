9043387f3bb309aaac8de2c10016a68370a218de
Akshay <Akshay@Akshays-MacBook-Pro.local>
2019-04-18 10:43:38 +0530

    Akshay changes 1

--- actions.py
+++ actions.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from rasa_core.actions.action import Action
 from rasa_core.events import SlotSet
-# import zomatopy
+import zomatopy
 import json
 
 class ActionSearchRestaurants(Action):
@@ -12,23 +12,24 @@ class ActionSearchRestaurants(Action):
 		return 'action_restaurant'
 		
 	def run(self, dispatcher, tracker, domain):
-		# config={ "user_key":"6ce88a5ec1419e335afa1c7f92f4b739"}
-		# zomato = zomatopy.initialize_app(config)
+		config={ "user_key":"f207a84eb81c174a12735f568cffd505"}
+		zomato = zomatopy.initialize_app(config)
 		loc = tracker.get_slot('location')
 		cuisine = tracker.get_slot('cuisine')
-		# location_detail=zomato.get_location(loc, 1)
-		# d1 = json.loads(location_detail)
-		# lat=d1["location_suggestions"][0]["latitude"]
-		# lon=d1["location_suggestions"][0]["longitude"]
-		# cuisines_dict={'bakery':5,'chinese':25,'cafe':30,'italian':55,'biryani':7,'north indian':50,'south indian':85}
-		# results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
-		# d = json.loads(results)
-		response="this is a test response"
-		# if d['results_found'] == 0:
-			# response= "no results"
-		# else:
-			# for restaurant in d['restaurants']:
-				# response=response+ "Found "+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+"\n"
+		price_range = tracker.get_slot('price')
+		location_detail=zomato.get_location(loc, 1)
+		d1 = json.loads(location_detail)
+		lat=d1["location_suggestions"][0]["latitude"]
+		lon=d1["location_suggestions"][0]["longitude"]
+		cuisines_dict={'chinese':25,'mexican':73,'south indian':85,'north indian':50,'american':1,'italian':55}
+		results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+		d = json.loads(results)
+		response=""
+		if d['results_found'] == 0:
+			response= "no results"
+		else:
+			for restaurant in d['restaurants']:
+				response=response+ "Restaurant Name: "+ restaurant['restaurant']['name']+ " Address: "+ restaurant['restaurant']['location']['address']+" Average budget for two people: "+restaurant['restaurant']['average_cost_for_two']+" Zomato user rating: "+ restaurant['restaurant']['user_rating']['aggregate_rating']+"\n"
 		
 		dispatcher.utter_message("-----"+response)
 		return [SlotSet('location',loc)]
